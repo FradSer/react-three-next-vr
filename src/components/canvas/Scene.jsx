9305ca5c2147dbc122240e7d04594079763012d8
Frad LEE <fradser@gmail.com>
2023-05-17 23:58:37 +0800

    feat(cmpts): add vr support

--- src/components/canvas/Scene.jsx
+++ src/components/canvas/Scene.jsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { VRButton, ARButton, XR, Controllers, Hands } from '@react-three/xr'
 import { Canvas } from '@react-three/fiber'
 import { Preload } from '@react-three/drei'
 import { r3f } from '@/helpers/global'
@@ -7,10 +8,17 @@ import { r3f } from '@/helpers/global'
 export default function Scene({ ...props }) {
   // Everything defined in here will persist between route changes, only children are swapped
   return (
-    <Canvas {...props}>
-      {/* @ts-ignore */}
-      <r3f.Out />
-      <Preload all />
-    </Canvas>
+    <>
+      <VRButton />
+      <Canvas {...props}>
+        <XR>
+          <Controllers />
+          <Hands />
+          {/* @ts-ignore */}
+          <r3f.Out />
+          <Preload all />
+        </XR>
+      </Canvas>
+    </>
   )
 }
